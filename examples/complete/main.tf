75120505d2f4afb2e808291f6161a6f374ba2c8e
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2024-01-29 14:04:13 +0530

    resolve review comments

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -79,11 +79,11 @@ resource "ibm_iam_access_group_members" "accgroupmem" {
 # Create Secrets Manager Instance
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.0.0"
+  version              = "1.1.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = local.sm_region
   secrets_manager_name = "${var.prefix}-secrets-manager"
-  sm_service_plan      = var.sm_service_plan
+  sm_service_plan      = "trial"
   service_endpoints    = "public-and-private"
   sm_tags              = var.resource_tags
 }
