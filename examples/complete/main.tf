b5b4ec6146e59380d04bd13c7c3324a62cd32000
Md Anam Raihan <md.anam.raihan17@gmail.com>
2026-05-08 20:10:25 +0530

    chore: update sm_tags to resource_tags (#489)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -108,12 +108,12 @@ resource "ibm_iam_access_group_members" "accgroupmem" {
 module "secrets_manager" {
   count                = var.existing_sm_instance_guid == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.14.2"
+  version              = "2.15.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = local.sm_region
   secrets_manager_name = "${var.prefix}-secrets-manager"
   sm_service_plan      = "trial"
-  sm_tags              = var.resource_tags
+  resource_tags        = var.resource_tags
 }
 
 # Add a Secrets Group to the secret manager instance
