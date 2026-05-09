c4b57841d300244f72e40f8b2d1cfefdbfc113f4
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-05-09 16:41:01 +0530

    chore(deps): update terraform terraform-ibm-modules/secrets-manager-secret-group/ibm to v1.5.2 (#492)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -119,7 +119,7 @@ module "secrets_manager" {
 # Add a Secrets Group to the secret manager instance
 module "secrets_manager_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.5.1"
+  version                  = "1.5.2"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-cloudant-secrets"
